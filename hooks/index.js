138bbcd7d557f9c7544a77cf8ef7b4a232dfd7fe
Shaurya <agarwal.lucky.shaurya11@gmail.com>
2021-10-23 16:18:28 +0530

    refactor: move import,export to `hooks/index.js`

---
+++ hooks/index.js
@@ -0,0 +1,9 @@
+export { default as useTest } from './useTest'
+export { default as useForm } from './useForm'
+export { default as useTodo } from './useTodo'
+export { default as useCounter } from './useCounter'
+export { default as useSet } from './useSet'
+export { default as useAxios } from './useAxios'
+export { default as useLocalStorage } from './useLocalStorage'
+export { default as useCookie } from './useCookie'
+
